b32d3feec387790ee953c56f9f770ed287ffacc6
erwantaupin <taupin.erwan@laposte.net>
2022-02-25 11:30:30 +0100

    finalcomit

---
+++ classementBL.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/styles/erwan.css" />
+    <link rel="stylesheet" href="assets/styles/navbarre.css">
+    <link rel="stylesheet" href="assets/styles/footer.css">
+    <title>classementBL</title>
+  </head>
+  <body>
+  <?php include("assets/includes/navbarre.php") ?>
+    <div class="titrec">
+      <h2>Brawlhala</h2>
+    </div>
+    <div class="classreturn">
+      <a href="formulaireBL.php" class="Buttonretc">Retour</a>
+    </div>
+    <div class="imgcjeux3"></div>
+    <div class="tournoi3">
+      <div class="sous-titre"><h3>Top 3</h3></div>
+      <div class="podium">
+        <div class="second1">
+          <div class="second-place3"></div>
+          <div class="second-place-name3">CEDRIC3</div>
+        </div>
+        <div class="first1">
+          <div class="first-place3"></div>
+          <div class="first-place-name3">QUENTIN3</div>
+        </div>
+        <div class="third1">
+          <div class="third-place3"></div>
+          <div class="third-place-name3">ERWAN3</div>
+        </div>
+      </div>
+    <div class="sous-titre"><h3>Classement</h3></div>
+    <div class="classement-tier3">
+      <ul>
+      <li><div class= "place3" id="place4">4<div id="name4">KYLIAN</div><div id="score4">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place5">5<div id="name5">ANONYME</div><div id="score5">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place6">6<div id="name6">ANONYME</div><div id="score6">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place7">7<div id="name7">ANONYME</div><div id="score7">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place8">8<div id="name8">ANONYME</div><div id="score8">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place9">9<div id="name9">ANONYME</div><div id="score9">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place10">10<div id="name10">ANONYME</div><div id="score10">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place11">11<div id="name11">ANONYME</div><div id="score11">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place12">12<div id="name12">ANONYME</div><div id="score12">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place13">13<div id="name13">ANONYME</div><div id="score13">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place14">14<div id="name14">ANONYME</div><div id="score14">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place15">15<div id="name15">ANONYME</div><div id="score15">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place16">16<div id="name16">ANONYME</div><div id="score16">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place17">17<div id="name17">ANONYME</div><div id="score17">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place18">18<div id="name18">ANONYME</div><div id="score18">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place19">19<div id="name19">ANONYME</div><div id="score19">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place20">20<div id="name20">ANONYME</div><div id="score20">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place21">21<div id="name21">ANONYME</div><div id="score21">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place22">22<div id="name22">ANONYME</div><div id="score22">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place23">23<div id="name23">ANONYME</div><div id="score23">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place24">24<div id="name24">ANONYME</div><div id="score24">#### POINTS</div></div></li>
+      <li><div class= "place3" id="place25">25<div id="name25">ANONYME</div><div id="score25">#### POINTS</div></div></li>
+    </ul>
+    </div>
+    <?php include("assets/includes/footer.php") ?>
+  </body>
+</html>
