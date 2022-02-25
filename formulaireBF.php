1feadf79dd4f2dc7d8657f21b342a29a9585ca4b
Nowrei <furlancedric@gmail.com>
2022-02-25 12:47:31 +0100

    mise en forme page

---
+++ formulaireBF.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href ="./assets/style/formulaireBF.css">
+</head>
+<body>
+ 
+
+<a href="#" class ="Buttonretc">Acceuil</a>
+
+
+
+<div class="formulaire">
+
+<div class="image">
+    
+</div>
+<div class ="description">
+<div class="container">
+	<section id="content">
+		<form action="">
+			<h1>Inscription</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" id="username" />
+			</div>
+			<div>
+				<input type="email" placeholder="E-mail" required="" id="email" />
+			</div>
+      <div>
+				<input type="phone" placeholder="Telephone" required="" id="telephone" />
+			</div>
+			<div>
+				<input type="submit" value="S'inscrire" href="#" />
+				
+			</div>
+		</form><!-- form -->
+		<div class="button">
+			<a href="#">Accès au classement</a>
+		</div><!-- button -->
+	</section><!-- content -->
+</div><!-- container -->
+</div>
+<div class="date"><p>Bro falls c'est un peu comme Fall guys mais en version wish et gratuite. Le jeu doit surement être pas mal mais l'artwork principal donne pas trop envie pour dire vrai, ça rend pas si bien.</p>
+</div>
+<div class="inscri ">   
+    <h2>Horraire du tournois</h2>
+    <p>Tout les Vendredi à partir de 19h : Phase de pool.<br>
+    Samedi à partir de 20h : Quart et Demi final<br>
+    Dimanche à partir de 19h : Finale jouer en bo7</p></div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
---
+++ formulaireBH.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href ="./assets/style/formulaireBH.css">
+</head>
+<body>
+ 
+
+<a href="ta_page.html" class ="Buttonretc">Acceuil</a>
+
+
+
+<div class="formulaire">
+
+<div class="image">
+    
+</div>
+<div class ="description">
+<div class="container">
+	<section id="content">
+		<form action="">
+			<h1>Inscription</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" id="username" />
+			</div>
+			<div>
+				<input type="email" placeholder="E-mail" required="" id="email" />
+			</div>
+      <div>
+				<input type="phone" placeholder="Telephone" required="" id="telephone" />
+			</div>
+			<div>
+				<input type="submit" value="S'inscrire" href="#" />
+				
+			</div>
+		</form><!-- form -->
+		<div class="button">
+			<a href="#">Accès au classement</a>
+		</div><!-- button -->
+	</section><!-- content -->
+</div><!-- container -->
+</div>
+<div class="date"><p>Sortie en free to play, Brawlhalla est un jeu de combat en 2D. Etant un smash like, Brawlhalla est un jeu de combat se déroulant sur divers plateformes, gardant le systemes d'arrène propre à Smash Bros.</p>
+</div>
+<div class="inscri ">   
+    <h2>Horraire du tournois</h2>
+    <p>Tout les Vendredi à partir de 19h : Phase de pool.<br>
+    Samedi à partir de 20h : Quart et Demi final<br>
+    Dimanche à partir de 19h : Finale jouer en bo7</p></div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
