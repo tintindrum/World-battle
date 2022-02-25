e74090cb33919aa55201cfac629d9e639d8df5fb
Nowrei <furlancedric@gmail.com>
2022-02-25 11:08:28 +0100

    formulaireCS

---
+++ formulaireCS.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href ="./assets/styles/cedric.css">
+</head>
+<body>
+ 
+
+<a href="ta_page.html" class ="Buttonretc">Acceuil</a>
+
+
+
+<div class="formulaire">
+
+<div class="image">
+    
+</div>
+<div class ="description">
+<div class="container">
+	<section id="content">
+		<form action="">
+			<h1>Login Form</h1>
+			<div>
+				<input type="text" placeholder="Username" required="" id="username" />
+			</div>
+			<div>
+				<input type="email" placeholder="E-mail" required="" id="email" />
+			</div>
+      <div>
+				<input type="phone" placeholder="Telephone" required="" id="telephone" />
+			</div>
+			<div>
+				<input type="submit" value="Log in" />
+				<a href="#"></a>
+				<a href="#"></a>
+			</div>
+		</form><!-- form -->
+		<div class="button">
+			<a href="#">Accès au classement</a>
+		</div><!-- button -->
+	</section><!-- content -->
+</div><!-- container -->
+</div>
+<div class="date"><p>Counter-Strike: Global Offensive (abrégé CS:GO) est un jeu de tir à la première personne mulitjoueur en ligne basé sur le jeu d'équipe développé par Valve Corporation. Il met en scène deux équipes de 5 joueurs. Les terroristes et les anti-terroristes. Les terroristes ayant pour but de placer une bombe et de la faire exploser tandis que les anti-terroriste doivent protéger les bombes sites. Dust 2, Mirage et Nuke étant bien entendu les meilleurs maps du jeu.</p>
+</div>
+<div class="inscri ">   
+    <h2>Horraire du tournois</h2>
+    <p>Tout les Vendredi à partir de 19h : Phase de pool.<br>
+    Samedi à partir de 20h : Quart et Demi final<br>
+    Dimanche à partir de 19h : Finale jouer en bo7</p></div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
